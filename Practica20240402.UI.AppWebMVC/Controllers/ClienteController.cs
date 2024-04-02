9aeceee8152aea272d87b542554e6b2d008ccd60
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2024-04-02 11:03:04 -0600

    graficos de barra y pastel en .net core 6

--- Practica20240402.UI.AppWebMVC/Controllers/ClienteController.cs
+++ Practica20240402.UI.AppWebMVC/Controllers/ClienteController.cs
@@ -92,5 +92,37 @@ namespace Practica20240402.UI.AppWebMVC.Controllers
                 return View();
             }
         }
+        public ActionResult GraficoPorEdades()
+        {           
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> GetInfoGraficoPorEdades()
+        {
+            var clientes = await _clienteBL.ObtenerTodos();
+            var yearActual = DateTime.Now.Year;
+            //2024-18 = 2012
+            // 2024-30 = 1994
+            var clienteEntre18y30 = clientes.Where(s => s.FechaNacimiento.Year> (yearActual-30) && s.FechaNacimiento.Year <= (yearActual - 18));
+            var clienteEntre31y50 = clientes.Where(s => s.FechaNacimiento.Year > (yearActual - 50) && s.FechaNacimiento.Year <= (yearActual - 30));
+            var clientemayoresa50 = clientes.Where(s => s.FechaNacimiento.Year > (yearActual - 200) && s.FechaNacimiento.Year <= (yearActual - 50));
+            var objs = new List<object>();
+            objs.Add(new
+            {
+                grupo = "Entre 18 y 30 años",
+                cantidad = clienteEntre18y30.Count(),
+            });
+            objs.Add(new
+            {
+                grupo = "Entre 30 y 50 años",
+                cantidad = clienteEntre31y50.Count(),
+            });
+            objs.Add(new
+            {
+                grupo = "Mayores a 50 años",
+                cantidad = clientemayoresa50.Count(),
+            });
+            return Json(objs);
+        }
     }
 }
